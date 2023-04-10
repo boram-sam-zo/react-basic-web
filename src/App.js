fcec05664b347660f8361738b87b17ea09839b4c
SayingGamja <schonlife99@gmail.com>
2023-04-10 15:43:58 +0900

    feat: Board component update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import Footer from './components/Footer';
-import Template from './components/Template';
-import RemoteControl from './components/RemoteControl';
-import TopBanner from './components/TopBanner';
-import Header from './components/Header';
-import MapGuide from './components/MapGuide';
-import ImagePhoto from './components/ImagePhoto';
-import ImageWrap from './components/ImageWrap';
+import "./App.css";
+import Footer from "./components/Footer";
+import Template from "./components/Template";
+import RemoteControl from "./components/RemoteControl";
+import TopBanner from "./components/TopBanner";
+import Header from "./components/Header";
+import MapGuide from "./components/MapGuide";
+import ImagePhoto from "./components/ImagePhoto";
+import ImageWrap from "./components/ImageWrap";
+import Board from "./components/Board";
 
 function App() {
   return (
@@ -17,7 +18,6 @@ function App() {
         <ImageWrap>
           <ImagePhoto />
         </ImageWrap>
-
         <MapGuide />
         <Footer />
       </Template>
---
+++ src/components/Board.js
@@ -0,0 +1,91 @@
+import React from "react";
+import "./Board.scss";
+import $ from "jquery";
+
+function Board() {
+  $(function () {
+    $(".board_title").click(function () {
+      $(".board_title").removeClass("on");
+      $(this).addClass("on");
+      $(".board_content").hide();
+      $("#" + $(this).data("type")).show();
+    });
+  });
+
+  return (
+    <div class="info">
+      <div class="content">
+        <div class="board_area">
+          <div class="board_title on" data-type="board_notice">
+            공지사항
+          </div>
+          <div class="board_title" data-type="board_data">
+            자료실
+          </div>
+          <div class="board_title" data-type="board_news">
+            뉴스
+          </div>
+          <div class="board_content" id="board_notice">
+            <ul>
+              <li>
+                공지사항입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                공지사항입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                공지사항입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                공지사항입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                공지사항입니다. <span>2023-01-01</span>
+              </li>
+            </ul>
+          </div>
+          <div class="board_content" id="board_data">
+            <ul>
+              <li>
+                자료실입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                자료실입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                자료실입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                자료실입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                자료실입니다. <span>2023-01-01</span>
+              </li>
+            </ul>
+          </div>
+          <div class="board_content" id="board_news">
+            <ul>
+              <li>
+                뉴스입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                뉴스입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                뉴스입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                뉴스입니다. <span>2023-01-01</span>
+              </li>
+              <li>
+                뉴스입니다. <span>2023-01-01</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
